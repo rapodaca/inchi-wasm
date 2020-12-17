84b718a168f8766a4617883140fb17fcb38847fe
hansonr <hansonr@STO28628W.ad.stolaf.edu>
2020-12-17 10:45:10 -0600

    upgrade for InChIKey -- untested starting point

--- src/molfile_to_inchi.c
+++ src/molfile_to_inchi.c
@@ -18,6 +18,19 @@ size_t molfile_to_inchi(char *molfile, char *options, char *result)
     return status;
 }
 
+size_t inchi_to_inchikey(char *inchi, char *result)
+{
+/* BH from mol2inchi.c */
+    int xhash1=0;
+    int xhash2=0;
+    char szXtra1[256], szXtra2[256];
+    int ik_ret = GetINCHIKeyFromINCHI(inchi, xhash1, xhash2, ikey, szXtra1, szXtra2);
+    if (ik_ret!=0)
+        ikey[0]='\0';
+    strcpy(ikey, result);
+    return ik_ret;
+}
+
 // see:
 // https://stackoverflow.com/questions/31476632/
 // https://stackoverflow.com/a/617606/54426
